e0330c076d2c25bc800b6c6b8d913c914072e301
JacksonImmunoJordenH <57252938+JacksonImmunoJordenH@users.noreply.github.com>
2019-11-08 13:23:04 +0000

    Added Scandinavia
    Added scandinavia and shortened javascript comments.

--- app.js
+++ app.js
@@ -6,18 +6,21 @@ dropDown.addEventListener("change", () => {
     // Checks the value of the selected drop down item, stores it into the "value" variable
     const value = dropDown.options[dropDown.selectedIndex].value;
 
-    // Store all elements with a class of "country-hide" into the "hideList" variable. This is stored as an array.
+    // Store all elements with a class of "country-hide" into the "hideList" variable.
     const hideList = document.querySelectorAll('.country-hide');
 
-    // Uses the value we stored earlier to find all elements with a class equal to that value, stores this information into the "showItem" variable. This is also stored as an array.
+    // Uses the value we stored earlier to find all elements with a class equal to that value
+    // stores this information into the "showItem" variable.
     const showItem = document.querySelectorAll(`.${value}`);
 
-    // Simple for loop that will run for all elements with the "country-hide" class. Sets the display to none to remove them completely from the flow of the page.
+    // Runs for all elements with the "country-hide" class. Sets the display to none 
+    // to remove them completely from the flow of the page.
     for (var i = 0; i< hideList.length; i++) {
         hideList[i].style.display = "none";
     }
 
-    // Another simple for loop. Runs for all elements that have a class equal to the value of the drop down select element. Set's the display to initial to reinstate the element into the flow of the page. Loop is used in case there are multiple elements with the class.
+    // Runs for all elements that have a class equal to the value of the drop down select element. 
+    // Set's the display to initial to reinstate the element into the flow of the page.
     for (var i = 0; i< showItem.length; i++) {
         showItem[i].style.display = "initial";
     }
